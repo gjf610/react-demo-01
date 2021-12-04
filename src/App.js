4d31dba4fdb6b3435fa5f223bfb8a0889285ed00
jianfeng <gjf610@yeah.net>
2021-12-04 15:33:27 +0800

    create react app demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+let number = 0
+let addButton = () => {
+    number += 1
+}
+let minusButton = () => {
+    number -= 1
+}
+const App = () => {
+    return (
+        <div className="parent">
+            <span className="red">{number}</span>
+            <button onClick={addButton}>+</button>
+            <button onClick={minusButton}>-</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css'
+//import App from './App.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <>
+      <Box1 name="加一" />
+      <Box2 name="加二" />
+    </>
+  )
+}
+class Box1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0
+    }
+  }
+  add() {
+    this.setState({ number: this.state.number += 1 })
+    // 优点：setState可以对更新进行优化，
+    // 如果下面写render函数会频繁调用render函数而导致页面卡顿
+    // 缺点：setState是异步更新
+    // 
+  }
+
+  minus() {
+    this.setState({ number: this.state.number -= 1 })
+  }
+  render() {
+    return (
+      <div className="parent">
+        我的name是{this.props.name}
+        <hr />
+        <span className="red">{this.state.number}</span>
+        <button onClick={this.add.bind(this)}>+</button>
+        {/* onClick.bind(undefind) */}
+
+        <button onClick={this.minus.bind(this)}>-</button>
+      </div>
+    )
+  }
+
+}
+class Box2 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0
+    }
+  }
+  add() {
+    this.setState({ number: this.state.number += 2 })
+  }
+  minus() {
+    this.setState({ number: this.state.number -= 2 })
+  }
+  render() {
+    return (
+      <div className="parent">
+        Box2 是{this.props.name}
+        <hr />
+        <span className="red">{this.state.number}</span>
+        <button onClick={this.add.bind(this)}>+</button>
+        <button onClick={this.minus.bind(this)}>-</button>
+      </div>
+    )
+  }
+}
+render()
+function render() {
+  ReactDOM.render(<App />,
+    document.getElementById('root')
+  );
+}
+
+
+
