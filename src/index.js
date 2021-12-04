2eaf7a1be3870b9f6dbd389aff11714a5f3ff055
jianfeng <gjf610@yeah.net>
2021-12-04 23:31:02 +0800

    added component style

--- src/index.js
+++ src/index.js
@@ -6,76 +6,46 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <Box1 name="加一" />
-      <Box2 name="加二" />
-    </>
+    <div className="App">
+      爸爸
+      <Son />
+    </div>
   )
 }
-class Box1 extends React.Component {
-  constructor(props) {
-    super(props)
+class Son extends React.Component {
+  constructor() {
+    super()
     this.state = {
-      number: 0
+      n: 0
     }
   }
   add() {
-    this.setState({ number: this.state.number += 1 })
-    // 优点：setState可以对更新进行优化，
-    // 如果下面写render函数会频繁调用render函数而导致页面卡顿
-    // 缺点：setState是异步更新
-    // 
+    this.setState({ n: this.state.n + 1 })
   }
-
   minus() {
-    this.setState({ number: this.state.number -= 1 })
+    this.setState({ n: this.state.n - 1 })
   }
   render() {
     return (
-      <div className="parent">
-        我的name是{this.props.name}
-        <hr />
-        <span className="red">{this.state.number}</span>
-        <button onClick={this.add.bind(this)}>+</button>
-        {/* onClick.bind(undefind) */}
-
-        <button onClick={this.minus.bind(this)}>-</button>
-      </div>
-    )
-  }
-
-}
-class Box2 extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      number: 0
-    }
-  }
-  add() {
-    this.setState({ number: this.state.number += 2 })
-  }
-  minus() {
-    this.setState({ number: this.state.number -= 2 })
-  }
-  render() {
-    return (
-      <div className="parent">
-        Box2 是{this.props.name}
-        <hr />
-        <span className="red">{this.state.number}</span>
-        <button onClick={this.add.bind(this)}>+</button>
-        <button onClick={this.minus.bind(this)}>-</button>
+      <div className="Son">
+        儿子 n:{this.state.n}
+        <button onClick={() => this.add()}>+</button>
+        <Grandson />
       </div>
     )
   }
 }
-render()
-function render() {
-  ReactDOM.render(<App />,
-    document.getElementById('root')
-  );
+const Grandson = () => {
+  const [n, setN] = React.useState(0)
+  return (
+    <div className="Grandson">
+      孙子 n:{n}
+      <button onClick={() => setN(n + 1)}>+</button>
+    </div>
+  )
 }
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 
 
